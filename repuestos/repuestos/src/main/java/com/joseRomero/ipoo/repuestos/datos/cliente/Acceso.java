86289905d2cf0f41dedc620fa96ad68a59db7df9
alumno-202 <alumno-202@M202-14.uaa.local>
2017-05-03 21:55:06 -0400

    acceso

---
+++ repuestos/repuestos/src/main/java/com/joseRomero/ipoo/repuestos/datos/cliente/Acceso.java
@@ -0,0 +1,15 @@
+package com.joseRomero.ipoo.repuestos.datos.cliente;
+
+import java.util.Date;
+
+public class Acceso {
+
+		private Date fechaHoraInicio;
+		private Date fechaHoraFin;
+		private String ip;
+		private cliente cliente;
+		
+		public Acceso(){
+			
+		}
+}
